2923428d7b023da1396273ef5798a69a4c904ae0
yusaazmi <yusaazmi24@gmail.com>
2021-12-20 13:15:18 +0700

    promises

--- index.js
+++ index.js
@@ -4,13 +4,18 @@ const superagent = require('superagent');
 fs.readFile(`${__dirname}/starter/dog.txt`, (err, data) => {
   console.log(`Breed:${data}`);
 
-  superagent.get(`https://dog.ceo/api/breed/${data}/images`).end((err, res) => {
-    if (err) return console.log(err.message);
-    console.log(res.body.message);
-    fs.writeFile(
-      './dog2.txt',
-      JSON.stringify(res.body.message),
-      function (err) {}
-    );
-  });
+  superagent
+    .get(`https://dog.ceo/api/breed/${data}/images`)
+    .then((res) => {
+      console.log(res.body.message);
+
+      fs.writeFile(
+        './dog2.txt',
+        JSON.stringify(res.body.message),
+        function (err) {}
+      );
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 });
